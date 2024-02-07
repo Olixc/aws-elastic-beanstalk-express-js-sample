d3cd8f8a13e72daca79f7173e930a25e28c7f1f0
olixpin <noibiabdulrasaq@gmail.com>
2024-02-06 16:55:27 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Beanstalk with aws build pipeline🔥"));
+app.get("/", (req, res) =>
+  res.send(
+    "Beanstalk with aws build pipeline🔥  Giving the review process to my pipeline🥹"
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
